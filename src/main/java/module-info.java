081f016c69e04266c326183e6e2d0b33666db9f5
Carlos <carlosmartz555@gmail.com>
2023-08-16 06:27:05 -0500

    removed all java fx references

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.carlos.datastructuresandalgorithms {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.carlos.datastructuresandalgorithms to javafx.fxml;
-    exports com.carlos.datastructuresandalgorithms;
-}
