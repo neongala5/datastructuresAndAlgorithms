3af4c0c7511e070b7e365ecb971b1c4abccd4ce5
Carlos <carlosmartz555@gmail.com>
2023-08-22 00:22:44 -0500

    valid parentheses solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/Stack/ValidParentheses.java
@@ -0,0 +1,27 @@
+package com.carlos.datastructuresandalgorithms.Stack;
+
+import java.util.Stack;
+
+class ValidParentheses {
+    public boolean isValid(String s) {
+        if(s.length() % 2 != 0) return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(Character c : s.toCharArray()){
+            if(c == '{' || c == '[' || c == '('){
+                stack.push(c);
+            } else if(c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if(c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if(c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
