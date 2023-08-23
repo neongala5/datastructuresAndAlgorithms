5f56ac27e4dfabf1bac4b20cb1e5abe08660e712
Carlos <carlosmartz555@gmail.com>
2023-08-23 01:17:46 -0500

    min stack solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/Stack/MinStack.java
@@ -0,0 +1,38 @@
+package com.carlos.datastructuresandalgorithms.Stack;
+
+import java.util.Stack;
+
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+
+    Stack<Integer> minStack = new Stack<>();
+
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+        stack.push(val);
+    }
+
+    public void pop() {
+        int minVal = minStack.peek();
+        int stackVal = stack.peek();
+        if(minVal == stackVal){
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
