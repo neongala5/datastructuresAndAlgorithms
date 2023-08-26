b7b2fd789a7406ff5fb0192d573eac9b3f9eaed7
Carlos <carlosmartz555@gmail.com>
2023-08-26 06:53:42 -0500

    binary search solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/BinarySearch/BinarySearch.java
@@ -0,0 +1,25 @@
+package com.carlos.datastructuresandalgorithms.BinarySearch;
+
+class BinarySearch {
+    public int search(int[] nums, int target) {
+
+        int pointer;
+        int leftMax = 0;
+        int rightMax = nums.length-1;
+
+        int resultIndex = -1;
+
+        while(leftMax <= rightMax){
+            pointer = (rightMax + leftMax) / 2;
+            if(nums[pointer] == target){
+                resultIndex = pointer;
+                return resultIndex;
+            } else if(nums[pointer] < target){
+                leftMax = pointer+1;
+            } else if(nums[pointer] > target){
+                rightMax = pointer-1;
+            }
+        }
+        return resultIndex;
+    }
+}
