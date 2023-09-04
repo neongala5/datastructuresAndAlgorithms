d76ba43563aaa99025beee7bd25f33000179230f
Carlos <carlosmartz555@gmail.com>
2023-09-04 08:40:24 -0500

    treenode class

--- src/main/java/com/carlos/datastructuresandalgorithms/BinaryTrees/InvertBinaryTree.java
+++ src/main/java/com/carlos/datastructuresandalgorithms/BinaryTrees/InvertBinaryTree.java
@@ -1,4 +1,14 @@
 package com.carlos.datastructuresandalgorithms.BinaryTrees;
 
-public class InvertBinaryTree {
+class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if( root == null) return root;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+        return root;
+    }
 }
