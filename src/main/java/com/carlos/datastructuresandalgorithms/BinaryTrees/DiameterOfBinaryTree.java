9f48ab7bf0f1a8ea468ab758cfdf6400c10d9563
Carlos <carlosmartz555@gmail.com>
2023-09-05 23:07:26 -0500

    diameter of tree solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/BinaryTrees/DiameterOfBinaryTree.java
@@ -0,0 +1,22 @@
+package com.carlos.datastructuresandalgorithms.BinaryTrees;
+
+class Solution {
+    int maxDiameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return maxDiameter;
+    }
+
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        int currentDiameter = leftDepth + rightDepth;
+        maxDiameter = Math.max(maxDiameter, currentDiameter);
+
+        return Math.max(leftDepth, rightDepth) +1;
+    }
+
+}
