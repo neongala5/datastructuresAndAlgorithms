975e6d82d6e6733275541ae1ae53677a8fa14a27
Carlos <carlosmartz555@gmail.com>
2023-09-07 02:03:09 -0500

    same tree solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/BinaryTrees/SameTree.java
@@ -0,0 +1,10 @@
+package com.carlos.datastructuresandalgorithms.BinaryTrees;
+
+class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(q == null && p == null) return true;
+        if(q == null || p == null || p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
