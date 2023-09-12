464546fd31efd5545f5ccec268630b520640c70e
Carlos <carlosmartz555@gmail.com>
2023-09-12 08:04:42 -0500

    format code

--- src/main/java/com/carlos/datastructuresandalgorithms/Stack/EvaluateReversePolishNotation.java
+++ src/main/java/com/carlos/datastructuresandalgorithms/Stack/EvaluateReversePolishNotation.java
@@ -4,7 +4,6 @@ import java.util.Stack;
 
 class EvaluateReversePolishNotation {
     public int evalRPN(String[] tokens) {
-
         Stack<String> stack = new Stack<>();
         int second;
         int first;
