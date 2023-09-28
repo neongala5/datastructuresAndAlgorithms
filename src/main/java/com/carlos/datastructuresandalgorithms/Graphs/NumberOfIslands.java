a5e09fd137dfd12590bbf31dd07a7b4a42ffc21d
Carlos <carlosmartz555@gmail.com>
2023-09-28 16:34:50 -0500

    number of islands solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/Graphs/NumberOfIslands.java
@@ -0,0 +1,29 @@
+package com.carlos.datastructuresandalgorithms.Graphs;
+
+class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+
+        int islandCount = 0;
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1') {
+                    islandCount++;
+                    dfs(i,j, grid);
+                }
+            }
+        }
+
+        return islandCount;
+    }
+
+    public void dfs(int i, int j, char[][] grid) {
+        if(i< 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') return;
+
+        grid[i][j] = '0';
+        dfs(i + 1, j, grid);
+        dfs(i - 1, j, grid);
+        dfs(i, j+ 1, grid);
+        dfs(i, j - 1, grid);
+    }
+}
