bf17185b77b296f7ec635fb6b34ce5bc7a336063
Carlos <carlosmartz555@gmail.com>
2023-10-07 17:59:54 -0500

    min cost climbing stairs solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/DynamicProgramming/MinCostClimbingStairs.java
@@ -0,0 +1,21 @@
+package com.carlos.datastructuresandalgorithms.DynamicProgramming;
+
+class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        //think can come up with better solution than this
+        // The array's length should be 1 longer than the length of cost
+        // This is because we can treat the "top floor" as a step to reach
+        int minimumCost[] = new int[cost.length + 1];
+
+        // Start iteration from step 2, since the minimum cost of reaching
+        // step 0 and step 1 is 0
+        for (int i = 2; i < minimumCost.length; i++) {
+            int takeOneStep = minimumCost[i - 1] + cost[i - 1];
+            int takeTwoSteps = minimumCost[i - 2] + cost[i - 2];
+            minimumCost[i] = Math.min(takeOneStep, takeTwoSteps);
+        }
+
+        // The final element in minimumCost refers to the top floor
+        return minimumCost[minimumCost.length - 1];
+    }
+}
