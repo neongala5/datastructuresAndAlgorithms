f85f5032099091407a6bb15552dc07486aec5e25
Carlos <carlosmartz555@gmail.com>
2023-10-08 22:03:56 -0500

    house robber solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/DynamicProgramming/HouseRobber.java
@@ -0,0 +1,21 @@
+package com.carlos.datastructuresandalgorithms.DynamicProgramming;
+
+class HouseRobber {
+    public int rob(int[] nums) {
+
+        int N = nums.length;
+
+        if (N == 0) { return 0;}
+
+        int[] maxRobbedAmount = new int[nums.length + 1];
+
+        maxRobbedAmount[N] = 0;
+        maxRobbedAmount[N - 1] = nums[N - 1];
+
+        for (int i = N - 2; i >= 0; --i) {
+            maxRobbedAmount[i] = Math.max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + nums[i]);
+        }
+
+        return maxRobbedAmount[0];
+    }
+}
