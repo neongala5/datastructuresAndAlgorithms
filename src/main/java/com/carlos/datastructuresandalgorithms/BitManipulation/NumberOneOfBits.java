75c7eff2ceafe6b596eb65a2b5b5bbbaa8fd07f0
Carlos <carlosmartz555@gmail.com>
2023-10-25 22:01:47 -0500

    nob sol

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/BitManipulation/NumberOneOfBits.java
@@ -0,0 +1,12 @@
+package com.carlos.datastructuresandalgorithms.BitManipulation;
+
+public class NumberOneOfBits {
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum++;
+            n &= (n - 1);
+        }
+        return sum;
+    }
+}
