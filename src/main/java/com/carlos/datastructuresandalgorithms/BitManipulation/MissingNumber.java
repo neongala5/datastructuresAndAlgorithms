c053b6ca3faba270b7718c50587856b519f3f4c1
Carlos <carlosmartz555@gmail.com>
2023-10-28 12:55:19 -0500

    missing number solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/BitManipulation/MissingNumber.java
@@ -0,0 +1,11 @@
+package com.carlos.datastructuresandalgorithms.BitManipulation;
+
+class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+}
