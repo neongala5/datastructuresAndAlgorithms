3c73e5e39e7e64a4ea56921f42b270a84ebb9d42
Carlos <carlosmartz555@gmail.com>
2023-11-14 01:18:26 -0600

    jump game solution

--- src/main/java/com/carlos/datastructuresandalgorithms/DatastructuresApplication.java
+++ src/main/java/com/carlos/datastructuresandalgorithms/DatastructuresApplication.java
@@ -6,6 +6,7 @@ public class DatastructuresApplication {
     public static void main(String[] args) {
         int[] nums = {1,2,3};
 
+
         ContainsDuplicates c = new ContainsDuplicates();
         System.out.println(c.containsDuplicate(nums));
         System.out.println("hello world");
