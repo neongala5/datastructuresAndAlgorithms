b05257bd8ab102c5278c60f33d8ee3c00e32bb9b
neongala5 <91646589+neongala5@users.noreply.github.com>
2023-11-25 05:07:23 -0600

    Update ContainsDuplicates.java

--- src/main/java/com/carlos/datastructuresandalgorithms/ArraysAndHashing/ContainsDuplicates.java
+++ src/main/java/com/carlos/datastructuresandalgorithms/ArraysAndHashing/ContainsDuplicates.java
@@ -2,7 +2,6 @@ package com.carlos.datastructuresandalgorithms.ArraysAndHashing;
 
 import java.util.HashSet;
 
-
 public class ContainsDuplicates {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> duplicateTester = new HashSet<Integer>();
