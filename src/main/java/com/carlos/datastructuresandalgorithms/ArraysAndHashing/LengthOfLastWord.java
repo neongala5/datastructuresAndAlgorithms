654463ed18ded937e86d6ca6fefe7b2aace1def1
Carlos <carlosmartz555@gmail.com>
2023-11-27 18:46:13 -0600

    length of last word solution

---
+++ src/main/java/com/carlos/datastructuresandalgorithms/ArraysAndHashing/LengthOfLastWord.java
@@ -0,0 +1,16 @@
+package com.carlos.datastructuresandalgorithms.ArraysAndHashing;
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+
+        int size = 0;
+
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(size > 0 && s.charAt(i) == ' ') break;
+            if(s.charAt(i) == ' ') continue;
+            size++;
+        }
+
+        return size;
+    }
+}
